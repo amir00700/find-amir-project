486edc01d84630517f957d194e7b1c360031af71
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-03-15 18:14:15 -0400

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
+    <>
+    <Particles
+    className="particles-canvas"
+    params={{
+      particles:{
+        number:{
+          value:30,
+          density:{
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "star",
+          stroke:{
+            width: 6,
+            color: "#DC143C"
+          }
+        }
+      }
+
+    }}
+      />
     <Navbar />
+    <Header/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 //react fontawesome imports
-import  {FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import  { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 
