894ccac364bc9855749f08c2fabc4f5f5915e951
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-03-16 17:50:49 -0400

    About Me Completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import AboutMe from "./components/AboutMe";
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
       />
     <Navbar />
     <Header/>
+    <AboutMe/>
     </>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,33 @@
+import React from "react";
+import author from "../aboutme.jpg"
+
+const AboutMe = () => {
+    return (
+        <div className="container py-5">
+            <div className="row">
+                <div className="col-lg-6 col-xm-12">
+                    
+                    <div className="photo-wrap mb-5">
+                    <img className="profile-img" src={author} alt="author..."/>
+
+                    </div>
+
+                </div>
+                <div className="col-lg-6 col-xm-12">
+                    <h1 className="about-heading">About me</h1>
+                    <p>Hellow! I am amir. I'm Full-Stack Web Developer. Technologies I uses is Mern(MongoDB, Express, ReactJS and NodeJS).
+                        I create responsive websites that are displayed on all devicec desktop and smartphones.
+                        I enjoy building everything small business sites to rich interactive web sites. If you are a busines seeking or a web presence or an employee looking to hire, you can get in touch with me.</p>
+                        <a href="#" className="btn-resume">Download Resume</a>
+                    
+                </div>
+    
+            </div>
+            
+
+            
+        </div>
+    )
+}
+
+export default AboutMe
